e198e8d147b4dad7ac72866e9ea6da80897d9ae4
Peter Theryo <theryop@outlook.com>
2016-09-28 20:07:35 -0400

    Update P1.asm
    Continued functionalities, further explained in comments

--- P1.asm
+++ P1.asm
@@ -3,6 +3,7 @@ myarrayA: .word 89,19,91,-5,23,-67,31,46,-71,-14,-10,3,67,17,11,-18,43,-73
 myarrayB: .word 0:11
 myarrayC: .word 0:7
 newLine: .asciiz "\n"
+messone: .asciiz "Index of the smallest value "
 .text
 	main:
 	add $s0, $zero, 100 #Min
@@ -12,6 +13,7 @@ newLine: .asciiz "\n"
 	#Clear $t0 to 0
 	addi $t0, $zero,0 #counters for loops
 	addi $t1, $zero,0
+	addi $t2, $zero,0
 	
 	while:
 		beq $t0 72, exit #If counter == 72, exit the while loop
@@ -22,20 +24,33 @@ newLine: .asciiz "\n"
 		
 		beq $s2, $zero, minset # If current array variable is smaller, minset
 		
-		blt $t6, $zero, lthan
+		blt $t6, $zero, lthan # if current array variable is negative, lthan
 		
-		bgt $t6, $zero, gthan
+		bgt $t6, $zero, gthan # if current arraty variable is positive, gthan
 		
-		addi $t0, $t0, 1
+		addi $t0, $t0, 4
 		
 		j while
-	minset:  
+	minset:   #Sets Min to the new minimum, and records the index
 		move $s0, $t6
 		move $t1, $t0
 		
-	lthan:
-		
-	gthan:	
+	lthan:  #sets the next negative arrayA variable into myArrayB
+		lw $t7 myarrayB($t1)
+		move $t7,$t6
+		sw $t7 myarrayB($t1)
+		addi $t1, $t1, 4
+	gthan:	#sets the next positive arrayA varialbe into myArrayC
+		lw $t8 myarrayC($t2)
+		move $t8, $t6
+		sw $t8 myarrayC($t2)
+		addi $t1, $t1, 4
 	
 	exit:
-	
+		li $v0, 4
+		la $a0, messone
+		syscall
+		
+		li $v0, 4
+		la $a0, $t1
+		syscall
